7ee00f7c2ccf0ea77fec06d5fdc6cf2a7c5cce49
yoredstorm <the.ares.p@gmail.com>
2026-05-17 05:09:37 -0500

    fix menor

--- frontend/components/betting/ChallengeModal.tsx
+++ frontend/components/betting/ChallengeModal.tsx
@@ -164,7 +164,7 @@ export function ChallengeModal({ fixtureId, open, onClose }: ChallengeModalProps
             }}
             onFocus={() => setShowSuggestions(true)}
             className="w-full bg-white/5 border border-white/10 rounded-lg px-3 py-2 text-white"
-            placeholder="ej. lcamacho"
+            placeholder="ej. ppimentel"
             autoComplete="off"
           />
           {showSuggestions && opponents && opponents.length > 0 && (
